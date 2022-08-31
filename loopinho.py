4cf12654f9999aac83f2e0c3f095f7a20f6f1744
Paiva <danpaiva@live.com>
2022-08-31 18:58:02 -0300

    Finish job

--- loopinho.py
+++ loopinho.py
@@ -1,9 +1,10 @@
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-# Welcome - Loopizinho #
+# Welcome - Loopizinho @danhpaiva #
 
 
 def clear():
@@ -17,32 +18,55 @@ def clear():
 
 def AcessGoogle(driver):
     driver.get('https://www.google.com')
-    time.sleep(3)
+    time.sleep(10)
 
 
 def AcessAzure(driver):
-    driver.get('https://azure.microsoft.com/')
-    time.sleep(3)
+    driver.get('https://docs.microsoft.com/pt-br/dotnet/azure/')
+    time.sleep(10)
 
 
 def GoToSearch(driver):
     inputButon = driver.find_element(
         By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
     inputButon.send_keys("consumir api soap c#" + Keys.ENTER)
-    time.sleep(5)
+    time.sleep(10)
+
+
+def RandomClick(driver):
+    number = random.randint(1, 4)
+    search = ''
+    if (number == 1):
+        search = 'performance net 7'
+    elif (number == 2):
+        search = 'kubernetes e docker com csharp'
+    elif (number == 3):
+        search = 'designe pattern c# guru'
+    else:
+        search = 'visual studio release notes'
+    inputButon = driver.find_element(
+        By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+    inputButon.send_keys(search + Keys.ENTER)
+    time.sleep(10)
 
 
 # Let's Code #
 while (True):
     driver = webdriver.Edge('.\edgedriver_win64\msedgedriver.exe')
-    AcessGoogle(driver)
-    GoToSearch(driver)
-    driver.back()
     time.sleep(2)
-    driver.close()
-    time.sleep(3)
-    driver = webdriver.Edge('.\edgedriver_win64\msedgedriver.exe')
-    AcessAzure(driver)
+    AcessGoogle(driver)
+    randomNumber = random.randint(1, 4)
+    if ((randomNumber % 2) == 0):
+        GoToSearch(driver)
+    else:
+        RandomClick(driver)
+    driver.back()
     time.sleep(5)
+    driver.close()
+    time.sleep(5)
+    driver = webdriver.Edge('.\edgedriver_win64\msedgedriver.exe')
+    time.sleep(2)
+    AcessAzure(driver)
     clear()
     driver.close()
+    time.sleep(5)
